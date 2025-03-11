1328577482a94cf00fbdfe993eb5a3d41464b1ea
h7x4 <h7x4@nani.wtf>
2025-03-11 18:41:54 +0100

    nixos/heimdal: correctly handle multitarget principals

--- nixos/modules/services/system/kerberos/heimdal.nix
+++ nixos/modules/services/system/kerberos/heimdal.nix
@@ -21,7 +21,10 @@ let
           target,
           ...
         }:
-        "${principal}\t${lib.concatStringsSep "," (lib.toList access)}\t${target}"
+        if target != "*" && target != null then
+          "${principal}\t${lib.concatStringsSep "," (lib.toList access)}\t${target}"
+        else
+          "${principal}\t${lib.concatStringsSep "," (lib.toList access)}"
       ) acl
     ))
     (lib.mapAttrsToList (
