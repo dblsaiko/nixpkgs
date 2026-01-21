6c9c80dfbc01f51b81808703dc84a2fd8491d524
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 15:15:30 +0100

    python3Packages.fastapi: 0.121.1 -> 0.128.0
    https://github.com/fastapi/fastapi/releases/tag/0.128.0

--- pkgs/development/python-modules/fastapi/default.nix
+++ pkgs/development/python-modules/fastapi/default.nix
@@ -43,7 +43,7 @@
 
 buildPythonPackage rec {
   pname = "fastapi";
-  version = "0.121.1";
+  version = "0.128.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -52,7 +52,7 @@ buildPythonPackage rec {
     owner = "tiangolo";
     repo = "fastapi";
     tag = version;
-    hash = "sha256-uUUARIHY8VBoLfWfMvveapypqiB00cTTWpJ4fi9nvUo=";
+    hash = "sha256-qUTSqTe9mQzfuwqsTCQY6u7Tcnh9XNy4tr5o0/qFFLs=";
   };
 
   build-system = [ pdm-backend ];
@@ -135,6 +135,8 @@ buildPythonPackage rec {
     # Don't test docs and examples
     "docs_src"
     "tests/test_tutorial/test_sql_databases"
+    # Infinite recursion with strawberry-graphql
+    "tests/test_tutorial/test_graphql/test_tutorial001.py"
   ];
 
   pythonImportsCheck = [ "fastapi" ];
