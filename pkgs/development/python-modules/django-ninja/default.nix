9db99bcc5e7a9e5ff31255cb249b7fa276f764aa
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-25 16:50:21 +0000

    python3Packages.django-ninja: 1.5.3 -> 1.6.2

--- pkgs/development/python-modules/django-ninja/default.nix
+++ pkgs/development/python-modules/django-ninja/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "django-ninja";
-  version = "1.5.3";
+  version = "1.6.2";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "vitalik";
     repo = "django-ninja";
     tag = "v${version}";
-    hash = "sha256-TmdwxOy+YwzEp/LrRb7/AqUahlmEodZdmswvfqtD5PY=";
+    hash = "sha256-nnGIhNGnK7q0nbw7EYJP+xCeS1uiuTrhQxf49dA+Sc8=";
   };
 
   build-system = [ flit-core ];
